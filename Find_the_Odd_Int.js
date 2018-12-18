b139212ddd812b13305a31fc4e5f04a7e8a73e00
Pin Chu <pichu0528@gmail.com>
2018-12-18 15:17:48 -0800

    Create Find_the_Odd_Int.js

---
+++ Find_the_Odd_Int.js
@@ -0,0 +1,19 @@
+/*
+Given an array, find the int that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+*/
+
+function findOdd(A) {
+  var c_count = {};                    // create an js object to keep track of the count of each character
+  for(var i = 0; i < A.length; i++){   // loop through the array to fill the js object
+    if(c_count[A[i]] === undefined)    // if the character is undefined in the object, assign 1 to the character key
+      c_count[A[i]] = 1;
+    else                               // else increment the count by 1
+      c_count[A[i]] += 1;
+  }
+
+  // use Number() method to convert the string value to integer
+  // use Object.keys() to find the list of key from the js object
+  // use Array.find() to find a particular key
+  return Number(Object.keys(c_count).find(key => c_count[key]%2 !== 0))
+}

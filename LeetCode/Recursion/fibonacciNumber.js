e16da4494dd56c711ff7dc0ade827a9a108e8501
Pin Chu <pichu0528@gmail.com>
2019-10-23 17:01:53 +0800

    Create fibonacciNumber.js

---
+++ LeetCode/Recursion/fibonacciNumber.js
@@ -0,0 +1,51 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+
+Example 1:
+Input: 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+Input: 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+URL: https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/1661/
+*/
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+
+// recusive method
+/*
+var fib = function(N) {
+    if(N < 2) return N;
+    
+    return fib(N-2)+fib(N-1);
+};
+*/
+
+// memoization method
+let n = {};
+var fib = function(N){
+    if(n[N]) return n[N]
+    
+    if (N < 2) return N;
+    
+    n[N] = fib(N-2) + fib(N-1);
+    
+    return n[N];
+}
